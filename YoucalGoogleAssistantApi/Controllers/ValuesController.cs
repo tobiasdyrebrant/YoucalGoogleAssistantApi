f886f34053e0655e8015083dc7a89294977c05ae
Thommy Lindahl <thommy.lindahl@digitalinformation.se>
2019-03-06 16:39:58 +0100

    test

--- YoucalGoogleAssistantApi/Controllers/ValuesController.cs
+++ YoucalGoogleAssistantApi/Controllers/ValuesController.cs
@@ -8,6 +8,10 @@ using Google.Cloud.Dialogflow.V2;
 using Google.Protobuf;
 using System.IO;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using Microsoft.Ajax.Utilities;
+using Microsoft.Build.Tasks.Deployment.Bootstrapper;
+using Newtonsoft.Json;
 
 
 namespace YoucalGoogleAssistantApi.Controllers
@@ -44,10 +48,17 @@ namespace YoucalGoogleAssistantApi.Controllers
 
         [Route("Test")]
         [HttpPost]
-        public async Task<IHttpActionResult> Index()
-        {
-           var response = new WebhookRequest();
-            return Ok("hej");
+        public async Task<IHttpActionResult> Index() {
+            dynamic test = "{'responseId':'asdassaddsasdddasdsad'," +
+                           " 'session': 'projects/resturant-search-agent-e0dd1/agent/sessions/88d13aa8-2999-4f71-b233-39cbf3a824a0'," +
+                           "'queryResult':{'queryText':'Find carwash close by?','parameters':'param':'param value'},"+
+                           "'allRequiredParamsPresent':true,"+
+                           "'fulfillmentText':'Hej'"+
+                           "'fulfillmentMessages':'[{'text': {'text': ['Text defined in Dialogflow's console for the intent that was matched']}]'"+
+                           "'outputContexts': [{'name': 'projects/resturant-search-agent-e0dd1/agent/sessions/88d13aa8-2999-4f71-b233-39cbf3a824a0/contexts/generic'," +
+                           "'intent': {'name': 'projects/resturant-search-agent-e0dd1/agent/intents/29bcd7f8-f717-4261-a8fd-2d3e451b8af8','displayName': 'Matched Intent Name'}'," +
+                           "'intentDetectionConfidence': 1,'diagnosticInfo': { },'languageCode': 'en'},'originalDetectIntentRequest': {}";
+            return Ok(test);
         }
     }
 }
