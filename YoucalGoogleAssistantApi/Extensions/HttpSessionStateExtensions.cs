e1dc0b18cfdac3a6194780e0cfff38faf5cd76fb
bdang92 <35771499+bdang92@users.noreply.github.com>
2019-03-11 15:38:48 +0100

    test change

---
+++ YoucalGoogleAssistantApi/Extensions/HttpSessionStateExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+namespace System.Web.SessionState
+{
+    /// <summary>
+    /// Extension methods for <see cref="HttpSessionState"/>
+    /// </summary>
+    [DebuggerStepThrough]
+    internal static class HttpSessionStateExtensions
+    {
+        /// <summary>
+        /// Tries to get a value from a session, if exists.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="session">The session.</param>
+        /// <param name="key">The key.</param>
+        /// <param name="value">The value.</param>
+        /// <returns></returns>
+        public static bool TryGet<T>(this HttpSessionState session, string key, out T value)
+        {
+            bool flag = false;
+            value = default(T);
+
+            if (session != null)
+            {
+                object objectValue = session[key];
+                if (flag = (objectValue != null && objectValue is T)) value = (T)objectValue;
+            }
+
+            return flag;
+        }
+    }
+}
---
+++ YoucalGoogleAssistantApi/Extensions/HttpStringExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace System.Web
+{
+    /// <summary>
+    /// Extension methods for <see cref="System.String"/> when used in System.Web
+    /// </summary>
+    [DebuggerStepThrough]
+    internal static class HttpStringExtensions
+    {
+        /// <summary>
+        /// Decodes a string from URL usage.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns></returns>
+        public static string UrlDecode(this string value)
+        {
+            if (value != null)
+            {
+                StringBuilder buffer = new StringBuilder(value);
+                buffer.Replace("--", "DASH");
+                buffer.Replace('-', ' ');
+                buffer.Replace("DASH", "-");
+                return HttpUtility.UrlDecode(buffer.ToString());
+            }
+            return value;
+        }
+
+        /// <summary>
+        /// Encodes a string for URL usage.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns></returns>
+        public static string UrlEncode(this string value)
+        {
+            if (value != null)
+            {
+                StringBuilder buffer = new StringBuilder(value);
+                buffer.Replace("-", "--");
+                buffer.Replace(' ', '-');
+                return HttpUtility.UrlEncode(buffer.ToString());
+            }
+            return value;
+        }
+    }
+}
