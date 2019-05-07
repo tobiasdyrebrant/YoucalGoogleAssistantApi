c3da3e42e4d0542a55e47a37072bcb5f987905f6
AlexanderSkarehag <Aleeex.S@gmail.com>
2019-05-07 15:46:16 +0200

    Bristfällig datumidentifierare baserat på användarinput.

--- YoucalGoogleAssistantApi/Controllers/ValuesController.cs
+++ YoucalGoogleAssistantApi/Controllers/ValuesController.cs
@@ -44,10 +44,10 @@ namespace YoucalGoogleAssistantApi.Controllers
         [Route("post")]
         public dynamic Post([FromBody]WebhookRequest dialogflowRequest)
         {
-            var intentName = dialogflowRequest.QueryResult.Intent.DisplayName; //hämtar ut specifik intent som callar post
-            var actualQuestion = dialogflowRequest.QueryResult.QueryText; //hämtar ut specifik fråga användaren ställer
-            var testAnswer = $"Dialogflow Request for intent '{intentName}' and question '{actualQuestion}'"; //testsvar för att se om vi kan få ut namn på intent och den frågan som ställts
-            var parameters = dialogflowRequest.QueryResult.Parameters;
+            //var intentName = dialogflowRequest.QueryResult.Intent.DisplayName; //hämtar ut specifik intent som callar post
+            //var actualQuestion = dialogflowRequest.QueryResult.QueryText; //hämtar ut specifik fråga användaren ställer
+            //var testAnswer = $"Dialogflow Request for intent '{intentName}' and question '{actualQuestion}'"; //testsvar för att se om vi kan få ut namn på intent och den frågan som ställts
+            //var parameters = dialogflowRequest.QueryResult.Parameters;
 
 
 
@@ -57,36 +57,56 @@ namespace YoucalGoogleAssistantApi.Controllers
             var antalBolag = foundcompanies.Count();
 
 
-            var testar = "hello this is a message going out to all my fellow Americans. Haikyuu might have been too powerful for us to comprehend the extent of the aftermath it would wield us";
+            var testar = "hello this is a message going out to all my fellow Americans. Haikyuu might have been too powerful for us to comprehend the extent of the aftermath it would wield us at 12/12";
 
-            for (int i = 0; i<= antalBolag; i++)
+            //for (int i = 0; i<= antalBolag; i++)
+            //{
+
+            //    var companyNameIndex = foundcompanies[i].Name;
+            //    if (testar.Contains(companyNameIndex)) //Byt ut testar mot actualQuestion för att få det i live versionen
+            //    {
+            //        var test = new Booking
+            //        {
+            //            Company = null,
+            //            Email = "a@a.a",
+            //            EndTime = Convert.ToDateTime("2019-12-13"),
+            //            StartTime = Convert.ToDateTime("2019-12-13"),
+            //            Phone = 999,
+            //            Price = 1000,
+            //            Username = "Ben10"
+            //        }; //Tas bort i framtiden till när vi kan få ut all data vi behöver till dialogflow, new booking ska ske på annat ställe.
+
+            //        db.Bookings.Add(test); // I framtiden spara till fält(?)
+            //        db.SaveChanges(); // I framtiden ta bort
+
+            //    }
+                
+            //}
+
+            var hasDate = false;
+            var whiteSpace = " ";
+            DateTime dateTime = new DateTime();
+            string[] inputText = testar.Split( whiteSpace.ToCharArray() );//split on a whitespace
+
+            foreach (string text in inputText)
             {
-
-                var companyNameIndex = foundcompanies[i].Name;
-                if (testar.Contains(companyNameIndex)) //Byt ut testar mot actualQuestion för att få det i live versionen
+                //Use the Parse() method
+                try
                 {
-                    var test = new Booking
-                    {
-                        Company = null,
-                        Email = "a@a.a",
-                        EndTime = Convert.ToDateTime("2019-12-13"),
-                        StartTime = Convert.ToDateTime("2019-12-13"),
-                        Phone = 999,
-                        Price = 1000,
-                        Username = "Ben10"
-                    }; //Tas bort i framtiden till när vi kan få ut all data vi behöver till dialogflow, new booking ska ske på annat ställe.
-
-                    db.Bookings.Add(test); // I framtiden spara till fält(?)
-                    db.SaveChanges(); // I framtiden ta bort
+                    dateTime = DateTime.Parse(text);
+                    hasDate = true;
+                    break;//no need to execute/loop further if you have your date
+                }
+                catch (Exception ex)
+                {
 
                 }
-                
             }
-            
-            
 
 
-          
+
+
+
             //WebhookResponse r = new WebhookResponse //skapar en ny webhookrespons med tillhörande textsvar, 
             //{
             //    FulfillmentText = testAnswer,
