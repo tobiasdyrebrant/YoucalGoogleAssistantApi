d1bf94bb6998a8731a3c6eb25f8677721010c4f1
AlexanderSkarehag <Aleeex.S@gmail.com>
2019-05-10 13:53:29 +0200

    Modifierade post så att den fungerar på dialogflow utan db

--- YoucalGoogleAssistantApi/Controllers/ValuesController.cs
+++ YoucalGoogleAssistantApi/Controllers/ValuesController.cs
@@ -17,6 +17,7 @@ using System.Web.Http.Results;
 using Newtonsoft.Json.Linq;
 using YoucalGoogleAssistantApi.DAL;
 using YoucalGoogleAssistantApi.Models;
+using System.Collections;
 
 //using System.Web.Mvc;
 
@@ -28,49 +29,188 @@ namespace YoucalGoogleAssistantApi.Controllers
         public YoucalContext db = new YoucalContext();
 
         
-        // POST api/values
+        // POST lokaldb
+        //[HttpPost]
+        //[Route("post")]
+        //public dynamic Post([FromBody]WebhookRequest dialogflowRequest)
+        //{
+        //    //Gäller Azure, går via dialogflow
+        //    //var intentName = dialogflowRequest.QueryResult.Intent.DisplayName; //hämtar ut specifik intent som callar post
+        //    //var actualQuestion = dialogflowRequest.QueryResult.QueryText; //hämtar ut specifik fråga användaren ställer
+        //    //var testAnswer = $"Dialogflow Request for intent '{intentName}' and question '{actualQuestion}'"; //testsvar för att se om vi kan få ut namn på intent och den frågan som ställts
+        //    //var parameters = dialogflowRequest.QueryResult.Parameters;
+
+        //    //Gäller local, kör via localDb
+        //    var foundcompanies = db.Companies.ToList();
+        //    var antalBolag = foundcompanies.Count();
+        //    var testar = "hello this is a message going out to all my fellow Americans. Haikyuu might have been too powerful for us to comprehend the extent of the aftermath it would wield us at 12/12";
+           
+        //    //vad som krävs för en bokning, ett företag och ett datum
+        //    var company = "";
+        //    var cID = 0;
+        //    var hasDate = false;
+        //    var cost = 0;
+        //    var realEndTime = new DateTime(2019, 12, 1);
+        //    try { 
+        //        for (int i = 0; i < antalBolag; i++)
+        //        {
+        //            var companyNameIndex = foundcompanies[i].Name;
+                
+        //            if (testar.Contains(companyNameIndex)) //Byt ut testar mot actualQuestion för att få det i live versionen
+        //            {
+        //                company = companyNameIndex;
+        //                cID = foundcompanies[i].CompanyID;
+        //                break;
+        //            }
+        //        }
+        //    }
+        //    catch(Exception e)
+        //    {
+        //        Console.WriteLine(e);
+        //    }
+
+        //    var whiteSpace = " ";
+        //    DateTime dateTime = new DateTime();
+        //    string[] inputText = testar.Split(whiteSpace.ToCharArray());//split on a whitespace
+
+        //    foreach (string text in inputText)
+        //    {
+        //        //Use the Parse() method
+        //        try
+        //        {
+        //            dateTime = DateTime.Parse(text) + new TimeSpan(8, 0, 0);
+        //            hasDate = true;
+        //            break;//no need to execute/loop further if you have your date
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            Console.WriteLine(ex.Message);
+        //        }
+        //    }
+
+
+        //    if (!company.IsNullOrWhiteSpace() && hasDate == true)
+        //    {
+        //        var companyId = db.Companies.Single(x => x.Name == company);
+        //        var serviceId = db.Provides.Single(x => x.CompanyID == companyId.CompanyID);
+        //        var endtime = new TimeSpan();
+        //        if (serviceId.Service.Type.Equals("Webdevelopment"))
+        //        {
+        //            endtime = new TimeSpan(12, 0, 0);
+        //            cost = 6000;
+        //        }
+        //        else if (serviceId.Service.Type.Equals("Painting"))
+        //        {
+        //            endtime = new TimeSpan(6, 0, 0);
+        //            cost = 600;
+        //        }
+        //        else if (serviceId.Service.Type.Equals("Hairy"))
+        //        {
+        //            endtime = new TimeSpan(0, 45, 0);
+        //            cost = 450;
+        //        }
+        //        else if (serviceId.Service.Type.Equals("Sewerline"))
+        //        {
+        //            endtime = new TimeSpan(0, 15, 0);
+        //            cost = 1000000;
+        //        }
+
+        //        realEndTime = dateTime.Add(endtime);
+
+        //        var test = new Booking
+        //        {
+        //            Company = companyId,
+        //            Email = "a@a.a",
+        //            EndTime = realEndTime,
+        //            StartTime = dateTime,
+        //            Phone = 999,
+        //            Service = serviceId.Service.Type,
+        //            Price = cost,
+        //            Username = "Ben10"
+        //        }; //Tas bort i framtiden till när vi kan få ut all data vi behöver till dialogflow, new booking ska ske på annat ställe.
+
+        //        db.Bookings.Add(test); // I framtiden spara till fält(?)
+        //        db.SaveChanges(); // I framtiden ta bort --- byt mellan connectionString beroende på om det gäller local eller Azure
+
+        //    }
+        //    else if (!company.IsNullOrWhiteSpace() && hasDate == false)
+        //    {
+        //        return Json(new
+        //        {
+        //            fulfillmentText = "Ops, seems like you're missing a valid date there. Mind doing that again but this time include the date as well?",
+        //            source = "Visual Studio",
+        //        });
+        //    }
+        //    else if (company.IsNullOrWhiteSpace() && hasDate == false)
+        //    {
+        //        return Json(new
+        //        {
+        //            fulfillmentText = "Ops, seems like you're missing not only a valid date, but also a valid company name. Mind doing that again but this time include the date and company as well?",
+        //            source = "Visual Studio",
+        //        });
+        //    }
+        //    else if (company.IsNullOrWhiteSpace() && hasDate == true)
+        //    {
+        //        return Json(new
+        //        {
+        //            fulfillmentText = "Ops, seems like you're missing a valid company name. Mind doing that again but this time include the company as well?",
+        //            source = "Visual Studio",
+        //        });
+        //    }
+
+        //    return Json(new
+        //    {
+        //        fulfillmentText = "Your booking at " + company + " is set to start at " + dateTime + " and end at " + realEndTime + ". Is there anything else you would like to book?",
+        //        source = "Visual Studio",
+
+
+        //    });
+        //}
+
+        // POST dialogflow
         [HttpPost]
         [Route("post")]
         public dynamic Post([FromBody]WebhookRequest dialogflowRequest)
         {
-            //Gäller Azure, går via dialogflow
-            //var intentName = dialogflowRequest.QueryResult.Intent.DisplayName; //hämtar ut specifik intent som callar post
-            //var actualQuestion = dialogflowRequest.QueryResult.QueryText; //hämtar ut specifik fråga användaren ställer
-            //var testAnswer = $"Dialogflow Request for intent '{intentName}' and question '{actualQuestion}'"; //testsvar för att se om vi kan få ut namn på intent och den frågan som ställts
-            //var parameters = dialogflowRequest.QueryResult.Parameters;
+            var intentName = dialogflowRequest.QueryResult.Intent.DisplayName; //hämtar ut specifik intent som callar post
+            var actualQuestion = dialogflowRequest.QueryResult.QueryText; //hämtar ut specifik fråga användaren ställer
+            var testAnswer = $"Dialogflow Request for intent '{intentName}' and question '{actualQuestion}'"; //testsvar för att se om vi kan få ut namn på intent och den frågan som ställts
+            var parameters = dialogflowRequest.QueryResult.Parameters;
 
             //Gäller local, kör via localDb
-            var foundcompanies = db.Companies.ToList();
-            var antalBolag = foundcompanies.Count();
-            var testar = "hello this is a message going out to all my fellow Americans. Haikyuu might have been too powerful for us to comprehend the extent of the aftermath it would wield us at 12/12";
-           
+            var foundcompanies = new ArrayList
+            {
+                "Peppes hairdresser",
+                "Mikes foodtruck",
+                "The repairman",
+                "MML delivery"
+            };
+            var antalBolag = foundcompanies.Count;
+            
             //vad som krävs för en bokning, ett företag och ett datum
             var company = "";
-            var cID = 0;
             var hasDate = false;
             var cost = 0;
-            var realEndTime = new DateTime(2019, 12, 1);
-            try { 
+            var realEndTime = new DateTime();
+            try
+            {
                 for (int i = 0; i < antalBolag; i++)
                 {
-                    var companyNameIndex = foundcompanies[i].Name;
-                
-                    if (testar.Contains(companyNameIndex)) //Byt ut testar mot actualQuestion för att få det i live versionen
+                    if (actualQuestion.Contains(foundcompanies[i].ToString())) //Byt ut testar mot actualQuestion för att få det i live versionen
                     {
-                        company = companyNameIndex;
-                        cID = foundcompanies[i].CompanyID;
+                        company = foundcompanies[i].ToString();
                         break;
                     }
                 }
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 Console.WriteLine(e);
             }
 
             var whiteSpace = " ";
             DateTime dateTime = new DateTime();
-            string[] inputText = testar.Split(whiteSpace.ToCharArray());//split on a whitespace
+            string[] inputText = actualQuestion.Split(whiteSpace.ToCharArray());//split on a whitespace
 
             foreach (string text in inputText)
             {
@@ -90,47 +230,29 @@ namespace YoucalGoogleAssistantApi.Controllers
 
             if (!company.IsNullOrWhiteSpace() && hasDate == true)
             {
-                var companyId = db.Companies.Single(x => x.Name == company);
-                var serviceId = db.Provides.Single(x => x.CompanyID == companyId.CompanyID);
                 var endtime = new TimeSpan();
-                if (serviceId.Service.Type.Equals("Webdevelopment"))
+                if (company.Equals("Peppes hairdresser"))
                 {
                     endtime = new TimeSpan(12, 0, 0);
                     cost = 6000;
                 }
-                else if (serviceId.Service.Type.Equals("Painting"))
+                else if (company.Equals("Mikes foodtruck"))
                 {
                     endtime = new TimeSpan(6, 0, 0);
                     cost = 600;
                 }
-                else if (serviceId.Service.Type.Equals("Hairy"))
+                else if (company.Equals("The repairman"))
                 {
                     endtime = new TimeSpan(0, 45, 0);
                     cost = 450;
                 }
-                else if (serviceId.Service.Type.Equals("Sewerline"))
+                else if (company.Equals("MML delivery"))
                 {
                     endtime = new TimeSpan(0, 15, 0);
                     cost = 1000000;
                 }
 
                 realEndTime = dateTime.Add(endtime);
-
-                var test = new Booking
-                {
-                    Company = companyId,
-                    Email = "a@a.a",
-                    EndTime = realEndTime,
-                    StartTime = dateTime,
-                    Phone = 999,
-                    Service = serviceId.Service.Type,
-                    Price = cost,
-                    Username = "Ben10"
-                }; //Tas bort i framtiden till när vi kan få ut all data vi behöver till dialogflow, new booking ska ske på annat ställe.
-
-                db.Bookings.Add(test); // I framtiden spara till fält(?)
-                db.SaveChanges(); // I framtiden ta bort --- byt mellan connectionString beroende på om det gäller local eller Azure
-
             }
             else if (!company.IsNullOrWhiteSpace() && hasDate == false)
             {
